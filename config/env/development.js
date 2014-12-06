9604013d3d8854a8ab4061a73096a2a750ffd838
Camilo Aguilar <camilo@acklenavenue.com>
2014-12-06 14:08:21 -0600

    updating with correct host

--- config/env/development.js
+++ config/env/development.js
@@ -14,7 +14,7 @@ module.exports = {
   facebook: {
     clientID: '330033637181364',
     clientSecret: 'b44643f67c9785dc063abd540525a2b4',
-    callbackURL: 'http://localhost:3000/auth/facebook/callback'
+    callbackURL: 'http://ukkkada13baf.kmiloaguilar.koding.io:3000/auth/facebook/callback'
   },
   twitter: {
     clientID: 'DEFAULT_CONSUMER_KEY',
