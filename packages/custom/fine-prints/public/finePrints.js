70f5781b2e1f17d3cf9c39dc14a770b8acc606fc
Richard Siwady <rsiwady29@gmail.com>
2014-12-06 23:26:18 -0600

    removed config ngclip

--- packages/custom/fine-prints/public/finePrints.js
+++ packages/custom/fine-prints/public/finePrints.js
@@ -1,6 +1,3 @@
 'use strict';
 
-angular.module('mean.fine-prints', [])
-    .config(['ngClipProvider', function (ngClipProvider) {
-        ngClipProvider.setPath('assets/ZeroClipboard/ZeroClipboard.swf');
-    }]);
+angular.module('mean.fine-prints', []);
