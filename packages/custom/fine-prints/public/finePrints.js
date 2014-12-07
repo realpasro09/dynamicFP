d17479a75033e5f813da2cea27d6e7e0d0759005
Richard Siwady <rsiwady29@gmail.com>
2014-12-06 22:38:42 -0600

    added clipboard config

--- packages/custom/fine-prints/public/finePrints.js
+++ packages/custom/fine-prints/public/finePrints.js
@@ -1,3 +1,6 @@
 'use strict';
 
-angular.module('mean.fine-prints', []);
+angular.module('mean.fine-prints', [])
+    .config(['ngClipProvider', function (ngClipProvider) {
+        ngClipProvider.setPath('assets/ZeroClipboard/ZeroClipboard.swf');
+    }]);
